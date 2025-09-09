7da04be92c0661c324d971a18c5be1f235e5e526
arancksj22 <arancksj@gmail.com>
2025-09-09 19:13:00 +0530

    imports

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("htesello")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from typing import List
